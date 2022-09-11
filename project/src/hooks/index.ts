29d2565d49ee2263848594d2216b66049d36b20b
Dmitry <banindm@mail.ru>
2022-09-12 00:57:44 +0300

    Updates

--- project/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
-import type { State, AppDispatch } from '../types/state';
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
--- project/src/store/action.ts
+++
@@ -1,6 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-import { AppRoute } from '../const';
-import { StateAction } from './action-types';
-
-export const redirectToRoute = createAction<AppRoute>(StateAction.User.RedirectToRoute);
-
